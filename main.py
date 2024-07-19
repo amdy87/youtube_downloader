782c66ff766f0eaf6a63cc8c82d6d2baf4a92ed2
austin <amoudy@wisc.edu>
2024-07-19 16:05:11 -0500

    Improved user flow

--- main.py
+++ main.py
@@ -9,13 +9,22 @@ _default_clients["IOS_MUSIC"][ "context"]["client"]["clientVersion"] = "6.41"
 _default_clients[ "ANDROID_MUSIC"] = _default_clients[ "ANDROID_CREATOR" ]
 
 def progress_func(stream,data,bytes_remain):
-    print(bytes_remain)
+    pass
+while(True):
+    yt = YouTube(input("Enter the url for the youtube video: "),on_progress_callback=progress_func)
 
-yt = YouTube(input("Enter the url for the youtube video: "),on_progress_callback=progress_func)
+    print("Video Title: ",yt.title)
+    yd = yt.streams.get_highest_resolution()
 
-print(yt.title)
-yd = yt.streams.get_highest_resolution()
+    print("Highest Resolution found: ",yd.resolution)
+    print("Video Framerate: ", yd.fps)
+    print("File Size: ",yd.filesize)
 
-yd.download()
+    confirm = input("Would you like to download this: ")[0]
 
+    if(confirm != 'y' and confirm != 'Y'):
+        continue
+
+    yd.download()
+    print("Downloaded")
 
